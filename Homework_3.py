6326160215062b99749ae8d6215b8da7d24d6059
MariiaZaskina <gestia29@gmail.com>
2021-07-03 15:31:36 +0300

    Update Homework_3.py

--- Homework_3.py
+++ Homework_3.py
@@ -8,26 +8,26 @@ driver.get("http://localhost/litecart/admin/")  # переход по ссылк
 driver.find_element_by_name("username").send_keys("admin")  # заполнение поля
 driver.find_element_by_name("password").send_keys("admin")  # заполнение поля
 driver.find_element_by_name("login").click()  # нажатие на кнопку
-wait = WebDriverWait(driver,10)
+wait = WebDriverWait(driver,10) # ожидание
 
-menu_number = len(driver.find_elements_by_css_selector("ul#box-apps-menu > li"))
+menu_number = len(driver.find_elements_by_css_selector("ul#box-apps-menu > li")) # список пунктов меню слева
 
-while menu_number:
+while menu_number: # цикл для нажатия пунктов меню слева
     menu_number -= 1
     menu_items = driver.find_elements_by_css_selector("ul#box-apps-menu > li")
     menu_items[menu_number].click()
 
-    def are_elements_present(driver, *args):
-        return len(driver.find_elements(*args)) > 0
+    def are_elements_present(driver, *args): # функция для проверки наличия заголовка
+        return len(driver.find_elements(*args)) > 0 # условие, что находится хотя бы один элемент
 
-    are_elements_present(driver, By.TAG_NAME, "h1") > 0
+    are_elements_present(driver, By.TAG_NAME, "h1") > 0 # проверка наличия заголовка
 
-    submenu_number = len(driver.find_elements_by_css_selector(".docs>li>a"))
+    submenu_number = len(driver.find_elements_by_css_selector(".docs>li>a")) # список вложенных пунктов меню слева
 
-    while submenu_number:
+    while submenu_number: # цикл для нажатия вложенных пунктов меню слева
         submenu_number -= 1
         submenu_items = driver.find_elements_by_css_selector(".docs>li>a")
         submenu_items[submenu_number].click()
-        are_elements_present(driver, By.TAG_NAME, "h1") > 0
+        are_elements_present(driver, By.TAG_NAME, "h1") > 0 # проверка наличия заголовка во вложенных пунктах
 
 
