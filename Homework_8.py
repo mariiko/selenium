b3fdff5ca085d13d0cee9a047976b0539266a343
MariiaZaskina <gestia29@gmail.com>
2021-07-18 13:21:14 +0300

    Update Homework_8.py

--- Homework_8.py
+++ Homework_8.py
@@ -13,6 +13,7 @@ driver.implicitly_wait(10)
 driver.find_element_by_css_selector("[href*=catalog]").click()
 driver.find_element_by_css_selector("[href*=edit_product]").click()
 
+
 def test_1():
     driver.find_element_by_css_selector("[value='0'][data-type='toggle']").click()
     driver.find_element_by_name("name[en]").send_keys("name_product")
@@ -25,9 +26,9 @@ def test_1():
     path = os.path.abspath("pic/peace.jpg")
     driver.find_element_by_css_selector("[type='file']").send_keys(path)
     driver.find_element_by_name("date_valid_from").send_keys(datetime.date.today().strftime('%d-%m-%Y'))
-    driver.find_element_by_name("date_valid_to").send_keys((datetime.date.today()+datetime.timedelta(days=30)).strftime('%d-%m-%Y'))
+    driver.find_element_by_name("date_valid_to").send_keys(
+        (datetime.date.today() + datetime.timedelta(days=30)).strftime('%d-%m-%Y'))
 
-    driver.implicitly_wait(10)
     driver.find_element_by_css_selector("[href='#tab-information']").click()
     Select(driver.find_element_by_name("manufacturer_id")).select_by_value("1")
     driver.find_element_by_name("keywords").send_keys("key_words_product")
@@ -36,7 +37,6 @@ def test_1():
     driver.find_element_by_name("head_title[en]").send_keys("head_title_product")
     driver.find_element_by_name("meta_description[en]").send_keys("meta_description_product")
 
-    driver.implicitly_wait(10)
     driver.find_element_by_css_selector("[href='#tab-prices']").click()
     driver.find_element_by_name("purchase_price").clear()
     driver.find_element_by_name("purchase_price").send_keys("100")
@@ -44,8 +44,11 @@ def test_1():
     driver.find_element_by_css_selector(".input-wrapper [name='prices[USD]']").send_keys("100")
     driver.find_element_by_css_selector(".input-wrapper [name='prices[EUR]']").send_keys("100")
     driver.find_element_by_css_selector("a#add-campaign").click()
-    driver.find_element_by_name("campaigns[new_1][start_date]").send_keys(datetime.date.today().strftime('%d-%m-%Y')+(Keys.ARROW_RIGHT)+(datetime.datetime.now().strftime('%H:%M')))
-    driver.find_element_by_name("campaigns[new_1][end_date]").send_keys((datetime.date.today()+datetime.timedelta(days=30)).strftime('%d-%m-%Y')+(Keys.ARROW_RIGHT)+(datetime.datetime.now().strftime('%H:%M')))
+    driver.find_element_by_name("campaigns[new_1][start_date]").send_keys(
+        datetime.date.today().strftime('%d-%m-%Y') + (Keys.ARROW_RIGHT) + (datetime.datetime.now().strftime('%H:%M')))
+    driver.find_element_by_name("campaigns[new_1][end_date]").send_keys(
+        (datetime.date.today() + datetime.timedelta(days=30)).strftime('%d-%m-%Y') + (Keys.ARROW_RIGHT) + (
+            datetime.datetime.now().strftime('%H:%M')))
     driver.find_element_by_name("campaigns[new_1][percentage]").clear()
     driver.find_element_by_name("campaigns[new_1][percentage]").send_keys("50")
 
